1fc0b060a28ac3ff757aeaedafe52f63dc6bcd68
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-19 13:55:48 -0600

    commit tree 984bf6298869e49662ad4492de37787319b18bab

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,13 +3,16 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  def new
-  end
+  def new; end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
